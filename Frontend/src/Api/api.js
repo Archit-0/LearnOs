e4dd3f6ad1398ee1e25d36d1664599d608b6dc65
Archit <archit0422chatan@gmail.com>
2025-08-09 12:23:23 +0530

    api url add

--- Frontend/src/Api/api.js
+++ Frontend/src/Api/api.js
@@ -1,7 +1,7 @@
 // api.js - Frontend API Service Client using Axios
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://learnos-olvx.onrender.com/api';
 
 class ApiService {
   constructor() {
