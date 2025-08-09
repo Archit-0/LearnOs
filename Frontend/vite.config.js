7a9aa6e86d9cadf4567bc885750fea2a33a18780
Archit <archit0422chatan@gmail.com>
2025-08-09 12:54:49 +0530

    vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,6 +6,9 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),   tailwindcss(),],
+  build: {
+    chunkSizeWarningLimit: 1000, // size in KB, e.g. 1000 KB = 1 MB
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
